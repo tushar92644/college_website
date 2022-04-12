166b9708f0a2b4176599a8edb1d9e63fe10d4adf
tushar92644 <75748268+tushar92644@users.noreply.github.com>
2022-04-12 10:23:13 +0530

    First commit

---
+++ form-handler.php
@@ -0,0 +1,13 @@
+<?php
+$name = $_post['name'];
+$visitor-email = $_post['email'];
+$subject = $_post['subject'];
+$message = $_post['message'];
+$email_from = 'singhaltushar@gmail.com';
+
+$email_subject = 'New Form Submission';
+
+$email_body = "User Name : $name.\n"
+
+
+?>
